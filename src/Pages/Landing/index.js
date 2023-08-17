23c68fb0d3fd5dafcca2c257bc47fff711d33942
mennatullah <mennaash5@gmail.com>
2023-08-17 15:06:34 +0300

    api

--- src/Pages/Landing/index.js
+++ src/Pages/Landing/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import Cards from '../../Components/Cardes'
+import CardContainer from '../../Components/CardContainer';
+// import { get } from '../../api'; 
 
 const Landing = () => {
 
-    //   useEffect()
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/MennatullahAsh/mockApi/house')
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => console.error('ERROR', error));
+    }, []);
 
-    return <>
-        {/* <p> welcome to landing page</p> */}
-        
-    </>
+    return (
+        <>
+{console.log(data, 'houses')}
+        <CardContainer houses={data}/>
+       
+        </>
+    );
 }
 
+
 export default Landing;
+
+
+
+
+
+
+
